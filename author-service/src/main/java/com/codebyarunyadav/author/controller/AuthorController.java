8173a46bdbe3c38ebde0dabab2b0a847d77e8d0a
hp <hp@DESKTOP-KNA879K>
2025-12-09 19:09:22 +0530

    @Tag(name = "Author API", description = "Authors CRUD operations")

--- author-service/src/main/java/com/codebyarunyadav/author/controller/AuthorController.java
+++ author-service/src/main/java/com/codebyarunyadav/author/controller/AuthorController.java
@@ -3,11 +3,14 @@ package com.codebyarunyadav.author.controller;
 import com.codebyarunyadav.author.model.Author;
 import com.codebyarunyadav.author.repo.AuthorRepository;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Author API", description = "Authors CRUD operations")
 @RestController
 @RequestMapping("/authors")
 public class AuthorController {
