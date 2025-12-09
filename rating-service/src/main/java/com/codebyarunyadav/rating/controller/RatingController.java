c31d33b6b72e31e25b7f4b66485c34d3142f7027
hp <hp@DESKTOP-KNA879K>
2025-12-09 19:06:50 +0530

    Tag(name = "Rating API", description = "Rating CRUD operations") to controller

--- rating-service/src/main/java/com/codebyarunyadav/rating/controller/RatingController.java
+++ rating-service/src/main/java/com/codebyarunyadav/rating/controller/RatingController.java
@@ -3,10 +3,13 @@ package com.codebyarunyadav.rating.controller;
 import com.codebyarunyadav.rating.model.Rating;
 import com.codebyarunyadav.rating.repo.RatingRepository;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Tag(name = "Rating API", description = "Rating CRUD operations")
 @RestController
 @RequestMapping("/ratings")
 public class RatingController {
