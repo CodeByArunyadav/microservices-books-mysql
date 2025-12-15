a3e5c3c38d12b9f92dbefad92a4daa9d50d8a1dd
hp <hp@DESKTOP-KNA879K>
2025-12-15 15:04:58 +0530

    OPTIONAL (we want to hard-force the URL)
    @OpenAPIDefinition(
        servers = {
            @Server(url = "http://localhost:8080", description = "API
    Gateway")
        }
    )

--- api-gateway/src/main/java/com/codebyarunyadav/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/codebyarunyadav/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,15 @@ package com.codebyarunyadav.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+	    servers = {
+	        @Server(url = "http://localhost:8080", description = "API Gateway")
+	    }
+	)
+
 @SpringBootApplication
 public class ApiGatewayApplication {
     public static void main(String[] args) {
