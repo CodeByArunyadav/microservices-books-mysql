2cede780354d7504ebc48fef471a73a3c1d12efd
Arun Yadav <codebyarunyadav@gmail.com>
2026-06-18 13:11:53 +0530

    Docker API Gateway server entry updated at each micro services

--- author-service/src/main/java/com/codebyarunyadav/author/config/OpenApiConfig.java
+++ author-service/src/main/java/com/codebyarunyadav/author/config/OpenApiConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
@@ -15,14 +18,23 @@ import io.swagger.v3.oas.models.servers.Server;
 public class OpenApiConfig {
 
 	@Bean
-	public OpenAPI customOpenAPI() {
+	public OpenAPI customOpenAPI()  { 
 
 		final String securitySchemeName = "bearerAuth";
 
 		return new OpenAPI()
-				.servers(List.of(new Server().url("http://localhost:8080/author-service").description("API Gateway")))
+
+				// API Information
+				.info(new Info().title("Author Management API").version("1.0.0")
+						.description("REST APIs for managing Author, Books and Rating " + "using Spring Boot 3 and MySQL.")
+
+						.contact(new Contact().name("HoxCloud Development Team").email("support@hoxcloud.com").url("https://hoxcloud.in"))
+
+						.license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
+				.servers(
+						List.of(new Server().url("http://localhost:8080/author-service").description("Local API Gateway"),
+								new Server().url("http://172.26.234.58:8080/author-service").description("Docker API Gateway")))
 				.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-				.components(new Components().addSecuritySchemes(securitySchemeName,
-						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+				.components(new Components().addSecuritySchemes(securitySchemeName,new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
 	}
 }
--- rating-service/src/main/java/com/codebyarunyadav/rating/config/OpenApiConfig.java
+++ rating-service/src/main/java/com/codebyarunyadav/rating/config/OpenApiConfig.java
@@ -1,16 +1,18 @@
 package com.codebyarunyadav.rating.config;
 
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 @Configuration
 public class OpenApiConfig {
 
@@ -20,9 +22,18 @@ public class OpenApiConfig {
 		final String securitySchemeName = "bearerAuth";
 
 		return new OpenAPI()
-				.servers(List.of(new Server().url("http://localhost:8080/rating-service").description("API Gateway")))
+
+				// API Information
+				.info(new Info().title("Rating Management API").version("1.0.0")
+						.description("REST APIs for managing Books Printing and Rating " + "using Spring Boot 3 and MySQL.")
+
+						.contact(new Contact().name("HoxCloud Development Team").email("support@hoxcloud.com").url("https://hoxcloud.in"))
+
+						.license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
+				.servers(
+						List.of(new Server().url("http://localhost:8080/rating-service").description("Local API Gateway"),
+								new Server().url("http://172.26.234.58:8080/rating-service").description("Docker API Gateway")))
 				.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-				.components(new Components().addSecuritySchemes(securitySchemeName,
-						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+				.components(new Components().addSecuritySchemes(securitySchemeName,new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
 	}
 }
